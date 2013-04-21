180e1ab230450eb976f442b27ac10fb115044446
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-21 14:59:48 +0100

    Chapter 2 - Circular ClockView
    Adds a circle that fills the bounds of the ClockView

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -9,6 +9,10 @@
  */
 package com.packtpub.e4.clock.ui.views;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
@@ -20,7 +24,14 @@ public class ClockView extends ViewPart {
 	 * Called by Eclipse 3.x to populate the view
 	 */
 	public void createPartControl(Composite parent) {
+		final Canvas clock = new Canvas(parent, SWT.NONE);
+		clock.addPaintListener(new PaintListener() {
+			public void paintControl(PaintEvent e) {
+				e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
+			}
+		});
 	}
+
 	/**
 	 * Called by Eclipse 3.x to notify the view has focus
 	 */
