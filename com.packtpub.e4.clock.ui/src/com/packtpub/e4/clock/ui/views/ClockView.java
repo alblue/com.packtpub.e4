2b2632d53894ca8482262c62135a425bbf0bd470
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-22 00:12:38 +0100

    Chapter 2 - Adding the TimeZone drop-down
    A Combo of all valid TimeZones is added to the ClockView, and focussed
    so that it picks up user input.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -9,11 +9,14 @@
  */
 package com.packtpub.e4.clock.ui.views;
 
+import java.util.TimeZone;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
@@ -23,6 +26,8 @@ import com.packtpub.e4.clock.ui.ClockWidget;
  * Provides a ClockView to show clocks
  */
 public class ClockView extends ViewPart {
+	private Combo timezones;
+
 	/**
 	 * Called by Eclipse 3.x to populate the view
 	 */
@@ -44,11 +49,18 @@ public class ClockView extends ViewPart {
 				0, 255));
 		clock1.setLayoutData(new RowData(20, 20));
 		clock3.setLayoutData(new RowData(100, 100));
+		String[] ids = TimeZone.getAvailableIDs();
+		timezones = new Combo(parent, SWT.SIMPLE);
+		timezones.setVisibleItemCount(5);
+		for (int i = 0; i < ids.length; i++) {
+			timezones.add(ids[i]);
+		}
 	}
 
 	/**
 	 * Called by Eclipse 3.x to notify the view has focus
 	 */
 	public void setFocus() {
+		timezones.setFocus();
 	}
 }
