9fd1732af890b7fa029ff94819d796dd7e32ca10
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-21 22:18:28 +0100

    Chapter 2 - Using FillLayout
    The RowLayout is used to show multiple clocks, and with the addition
    of specific RowData, to customise the elements within the layout.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -10,7 +10,8 @@
 package com.packtpub.e4.clock.ui.views;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
@@ -25,8 +26,13 @@ public class ClockView extends ViewPart {
 	 */
 	@SuppressWarnings("unused")
 	public void createPartControl(Composite parent) {
-		final Canvas clock = new ClockWidget(parent, SWT.NONE);
-	}
+		RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+		parent.setLayout(layout);
+		final ClockWidget clock1 = new ClockWidget(parent, SWT.NONE);
+		final ClockWidget clock2 = new ClockWidget(parent, SWT.NONE);
+		final ClockWidget clock3 = new ClockWidget(parent, SWT.NONE);
+		clock1.setLayoutData(new RowData(20,20));
+		clock3.setLayoutData(new RowData(100,100));	}
 
 	/**
 	 * Called by Eclipse 3.x to notify the view has focus
