b6fa7118654b25245981bfbe1553758da42d32c2
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-21 17:00:28 +0100

    Chapter 2 - Drawing the second hand
    Draw a second hand onto the clock face with a fillArc() call, such that
    when the view is redrawn that the second hand is shown.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -9,9 +9,12 @@
  */
 package com.packtpub.e4.clock.ui.views;
 
+import java.util.Date;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
@@ -28,6 +31,11 @@ public class ClockView extends ViewPart {
 		clock.addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
 				e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
+				int seconds = new Date().getSeconds();
+				int arc = (15 - seconds) * 6 % 360;
+				Color blue = e.display.getSystemColor(SWT.COLOR_BLUE);
+				e.gc.setBackground(blue);
+				e.gc.fillArc(e.x, e.y, e.width - 1, e.height - 1, arc - 1, 2);
 			}
 		});
 	}
