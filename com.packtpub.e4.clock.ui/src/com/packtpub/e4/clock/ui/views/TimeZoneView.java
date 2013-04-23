6c12ad7077554838716b802ca9f6c9406109ab2a
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-23 19:51:15 +0100

    Chapter 2 - Adding ClockWidgets to the TimeZoneView
    This change adds a list of ClockWidgets to the TimeZoneView, so that
    each tab displays clocks in the time zones for that region.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneView.java
@@ -18,27 +18,43 @@ import java.util.TimeZone;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+import com.packtpub.e4.clock.ui.ClockWidget;
 import com.packtpub.e4.clock.ui.internal.TimeZoneComparator;
 
 public class TimeZoneView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		  Map<String, Set<TimeZone>> timeZones = TimeZoneComparator.getTimeZones();
-		  CTabFolder tabs = new CTabFolder(parent, SWT.BOTTOM);
-		  Iterator<Entry<String, Set<TimeZone>>> regionIterator =
-		   timeZones.entrySet().iterator();
-		  while (regionIterator.hasNext()) {
-		    Entry<String, Set<TimeZone>> region =
-		     regionIterator.next();
-		    CTabItem item = new CTabItem(tabs, SWT.NONE);
-		    item.setText(region.getKey());
-		  }
-		  tabs.setSelection(0);
+		Map<String, Set<TimeZone>> timeZones = TimeZoneComparator
+				.getTimeZones();
+		CTabFolder tabs = new CTabFolder(parent, SWT.BOTTOM);
+		Iterator<Entry<String, Set<TimeZone>>> regionIterator = timeZones
+				.entrySet().iterator();
+		while (regionIterator.hasNext()) {
+			Entry<String, Set<TimeZone>> region = regionIterator.next();
+			CTabItem item = new CTabItem(tabs, SWT.NONE);
+			item.setText(region.getKey());
+			Composite clocks = new Composite(tabs, SWT.NONE);
+			item.setControl(clocks);
+			clocks.setLayout(new RowLayout());
+			RGB rgb = new RGB(128, 128, 128);
+			TimeZone td = TimeZone.getDefault();
+			Iterator<TimeZone> timezoneIterator = region.getValue().iterator();
+			while (timezoneIterator.hasNext()) {
+				TimeZone tz = timezoneIterator.next();
+				ClockWidget clock = new ClockWidget(clocks, SWT.NONE, rgb);
+				clock.setOffset((tz.getOffset(System.currentTimeMillis()) - td
+						.getOffset(System.currentTimeMillis())) / 3600000);
+			}
 		}
+		tabs.setSelection(0);
+	}
+
 	@Override
 	public void setFocus() {
 	}
