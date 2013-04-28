6a6bcdcbd8062b9401e4182030d1daef904d4dd9
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-28 14:51:38 +0100

    Chapter 3 - Creating a TimeZoneTableView
    Create an empty TimeZoneTableView with a table consisting of
    TimeZone IDs.

---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTableView.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2013, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.clock.ui.views;
+
+import java.util.TimeZone;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class TimeZoneTableView extends ViewPart {
+	private TableViewer tableViewer;
+
+	public void createPartControl(Composite parent) {
+		tableViewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+		tableViewer.getTable().setHeaderVisible(true);
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		tableViewer.setInput(TimeZone.getAvailableIDs());
+	}
+
+	public void setFocus() {
+		tableViewer.getControl().setFocus();
+	}
+
+}
