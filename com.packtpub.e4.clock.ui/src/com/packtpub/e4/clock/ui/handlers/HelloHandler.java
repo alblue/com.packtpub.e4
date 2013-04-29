017b81befd56cfec14b8ea2a9fdba450ee75b64c
Alex Blewitt <alex.blewitt@gmail.com>
2013-04-29 23:37:46 +0100

    Chapter 4 - Adding a handler and menu item
    Add a Hello handler and an associated menu item to invoke the command.

---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2013, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2013, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.clock.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class HelloHandler extends AbstractHandler {
+	public Object execute(ExecutionEvent event) {
+		MessageDialog.openInformation(null, "Hello", "World");
+		return null;
+	}
+}
