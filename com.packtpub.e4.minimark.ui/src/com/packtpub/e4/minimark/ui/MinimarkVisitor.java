8d1e5bfbc0234db05c75345b94b436b83e78b7cf
Alex Blewitt <alex.blewitt@gmail.com>
2013-05-04 13:43:31 +0100

    Chapter 6 - Creating resources in a builder
    When the build occurs, instead of displaying the generated output in the
    host's console, create a resource and set the contents on it
    automatically.
    
    This currently only works will a full build, triggered with
    Project->Clean.

--- com.packtpub.e4.minimark.ui/src/com/packtpub/e4/minimark/ui/MinimarkVisitor.java
+++ com.packtpub.e4.minimark.ui/src/com/packtpub/e4/minimark/ui/MinimarkVisitor.java
@@ -9,11 +9,14 @@
  */
 package com.packtpub.e4.minimark.ui;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
@@ -21,6 +24,7 @@ import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.IResourceProxy;
 import org.eclipse.core.resources.IResourceProxyVisitor;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 
 public class MinimarkVisitor implements IResourceProxyVisitor,
@@ -35,8 +39,6 @@ public class MinimarkVisitor implements IResourceProxyVisitor,
 	public boolean visit(IResourceProxy proxy) throws CoreException {
 		String name = proxy.getName();
 		if (name != null && name.endsWith(".minimark")) {
-			// found a source file
-			System.out.println("Processing " + name);
 			processResource(proxy.requestResource());
 		}
 		return true;
@@ -46,9 +48,21 @@ public class MinimarkVisitor implements IResourceProxyVisitor,
 		if (resource instanceof IFile) {
 			try {
 				IFile file = (IFile) resource;
+				String htmlName = file.getName().replace(".minimark", ".html");
+				IContainer container = file.getParent();
+				IFile htmlFile = container.getFile(new Path(htmlName));
 				InputStream in = file.getContents();
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
 				MinimarkTranslator.convert(new InputStreamReader(in),
-						new OutputStreamWriter(System.out));
+						new OutputStreamWriter(baos));
+				ByteArrayInputStream contents = new ByteArrayInputStream(
+						baos.toByteArray());
+				if (htmlFile.exists()) {
+					htmlFile.setContents(contents, true, false, null);
+				} else {
+					htmlFile.create(contents, true, null);
+				}
+				htmlFile.setDerived(true,null);
 			} catch (IOException e) {
 				throw new CoreException(new Status(Status.ERROR,
 						Activator.PLUGIN_ID, "Failed to generate resource", e));
