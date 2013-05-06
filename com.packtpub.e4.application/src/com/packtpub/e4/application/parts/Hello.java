ab544e0b6babc5350c09a7df24d22d6f2d1c11b0
Alex Blewitt <alex.blewitt@gmail.com>
2013-05-06 22:08:37 +0100

    Chapter 7 - Preferences and E4
    Eclipse4 provides a mechanism to inject both a preference store as well
    as individual preference fields using the @Preference annotation.

--- com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
@@ -13,7 +13,9 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
@@ -22,6 +24,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.osgi.service.log.LogService;
+import org.osgi.service.prefs.BackingStoreException;
 
 @SuppressWarnings("restriction")
 public class Hello {
@@ -34,11 +37,17 @@ public class Hello {
 	@Inject
 	@Named("math.random")
 	private Object random;
+	@Inject
+	@Preference(nodePath = "com.packtpub.e4.application", value = "greeting")
+	private String greeting;
+	@Inject
+	@Preference(nodePath = "com.packtpub.e4.application")
+	private IEclipsePreferences prefs;
 
 	@PostConstruct
 	public void create(Composite parent) {
 		label = new Label(parent, SWT.NONE);
-		label.setText(window.getLabel() + " " + random);
+		label.setText(greeting + " " + window.getLabel() + " " + random);
 		if (log != null) {
 			log.log(LogService.LOG_ERROR, "Hello");
 		}
@@ -60,9 +69,22 @@ public class Hello {
 
 	@Inject
 	@Optional
-	public void receiveEvent(@UIEventTopic("rainbow/colour") String data) {
+	public void receiveEvent(@UIEventTopic("rainbow/colour") String data)
+			throws BackingStoreException {
 		// This needs to use @UIEventTopic
 		// If @EventTopic is used it will not run on the UI thread
 		label.setText(data);
+		prefs.put("greeting", "I like " + data);
+		prefs.sync();
+	}
+
+	@Inject
+	@Optional
+	void setText(
+			@Preference(nodePath = "com.packtpub.e4.application", value = "greeting") String text) {
+		if (text != null && label != null && !label.isDisposed()) {
+			// NB Run in UI thread!
+			label.setText(text);
+		}
 	}
 }
