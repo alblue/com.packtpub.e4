c2ea0808db7d443e01ba03c59eb7f7c0bc4934ed
Alex Blewitt <alex.blewitt@gmail.com>
2013-05-07 22:04:13 +0100

    Chapter 8 - Creating a headless application
    This creates an extension based on the applications extension point that
    prints out a message when invoked.

---
+++ com.packtpub.e4.headless.application/src/com/packtpub/e4/headless/application/Activator.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2013, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2013, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.headless.application;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ com.packtpub.e4.headless.application/src/com/packtpub/e4/headless/application/Application.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2013, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2013, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.headless.application;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class Application implements IApplication {
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		System.out.println("Headless Application");
+		return null;
+	}
+	@Override
+	public void stop() {
+	}
+}
