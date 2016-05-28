6b0eda587e7299979c18df743e6d808c8e5f913b
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-28 23:25:09 +0100

    Chapter 2 - Drawing the second hand
    Draw a second hand onto the clock face with a fillArc() call, such that
    when the view is redrawn that the second hand is shown.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -9,9 +9,11 @@
  */
 package com.packtpub.e4.clock.ui.views;
 
+import java.time.LocalTime;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
@@ -19,11 +21,16 @@ import org.eclipse.ui.part.ViewPart;
 public class ClockView extends ViewPart {
 	public void createPartControl(Composite parent) {
 		final Canvas clock = new Canvas(parent, SWT.NONE);
-		clock.addPaintListener(new PaintListener() {
-			public void paintControl(PaintEvent e) {
-				e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
-			}
-		});
+		clock.addPaintListener(this::drawClock);
+	}
+
+	private void drawClock(PaintEvent e) {
+		e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
+		int seconds = LocalTime.now().getSecond();
+		int arc = (15 - seconds) * 6 % 360;
+		Color blue = e.display.getSystemColor(SWT.COLOR_BLUE);
+		e.gc.setBackground(blue);
+		e.gc.fillArc(e.x, e.y, e.width - 1, e.height - 1, arc - 1, 2);
 	}
 
 	public void setFocus() {
