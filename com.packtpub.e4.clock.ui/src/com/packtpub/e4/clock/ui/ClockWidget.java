0ee505a4db063d0bef9a6f4001f816ce97b936f2
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-29 00:46:54 +0100

    Chapter 2 - Colouring the ClockWidget
    This adds an RGB value to the ClockWidget's constructor, which is used
    to create a Color instance when the widget is created.
    
    Note that at this point the Color is leaked when the widget is disposed.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockWidget.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockWidget.java
@@ -15,12 +15,17 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 public class ClockWidget extends Canvas {
-	public ClockWidget(Composite parent, int style) {
+	private final Color color;
+
+	public ClockWidget(Composite parent, int style, RGB rgb) {
 		super(parent, style);
+		// FIXME color is leaked!
+		this.color = new Color(parent.getDisplay(), rgb);
 		addPaintListener(this::drawClock);
 		Runnable redraw = () -> {
 			while (!this.isDisposed()) {
@@ -40,8 +45,7 @@ public class ClockWidget extends Canvas {
 		e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
 		int seconds = LocalTime.now().getSecond();
 		int arc = (15 - seconds) * 6 % 360;
-		Color blue = e.display.getSystemColor(SWT.COLOR_BLUE);
-		e.gc.setBackground(blue);
+		e.gc.setBackground(color);
 		e.gc.fillArc(e.x, e.y, e.width - 1, e.height - 1, arc - 1, 2);
 	}
 
--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -10,6 +10,7 @@
 package com.packtpub.e4.clock.ui.views;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -21,9 +22,9 @@ public class ClockView extends ViewPart {
 	public void createPartControl(Composite parent) {
 		RowLayout layout = new RowLayout(SWT.HORIZONTAL);
 		parent.setLayout(layout);
-		final ClockWidget clock1 = new ClockWidget(parent, SWT.NONE);
-		final ClockWidget clock2 = new ClockWidget(parent, SWT.NONE);
-		final ClockWidget clock3 = new ClockWidget(parent, SWT.NONE);
+		final ClockWidget clock1 = new ClockWidget(parent, SWT.NONE, new RGB(255, 0, 0));
+		final ClockWidget clock2 = new ClockWidget(parent, SWT.NONE, new RGB(0, 255, 0));
+		final ClockWidget clock3 = new ClockWidget(parent, SWT.NONE, new RGB(0, 0, 255));
 		clock1.setLayoutData(new RowData(20, 20));
 		clock2.setLayoutData(new RowData(50, 50));
 		clock3.setLayoutData(new RowData(100, 100));
