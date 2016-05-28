7462e32607f87c39ecfd30beb5f52335121dfe5b
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-28 22:31:46 +0100

    Chapter 1 - Custom Breakpoint utility class
    By using the custom breakpoint utility class, a richer set of logging
    information can be obtained from the debugger, without causing the
    debugger to stop. The assosciated breakpoint can be used to test this
    class.

---
+++ com.packtpub.e4.hello.ui/src/com/packtpub/e4/hello/ui/handlers/Utility.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.hello.ui.handlers;
+
+public class Utility {
+	public static boolean breakpoint(String msg, Object... args) {
+		return breakpoint(true, msg, args);
+	}
+
+	public static boolean breakpoint(boolean stop, String msg, Object... args) {
+		if (args.length == 0) {
+			System.out.println(msg);
+		} else {
+			System.out.println(String.format(msg, args));
+		}
+		return stop;
+	}
+}
