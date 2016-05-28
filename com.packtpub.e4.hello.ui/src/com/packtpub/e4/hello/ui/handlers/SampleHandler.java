84db466ae12f0ce0af15ca67cf98771df1b95abc
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-28 17:15:01 +0100

    Chapter 1 - Creating your first plug-in
    A new plug-in project can be created with File -> New -> Project...
    and choosing the Plug-in Project type. The samples allow a
    Hello, World Command handler to be created.

---
+++ com.packtpub.e4.hello.ui/src/com/packtpub/e4/hello/ui/handlers/SampleHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.hello.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class SampleHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Hello",
+				"Hello, Eclipse world");
+		return null;
+	}
+}
