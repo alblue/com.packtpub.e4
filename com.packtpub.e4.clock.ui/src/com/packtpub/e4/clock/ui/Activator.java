850ef22f909ced5fb186b92599a6e7da18deb66e
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-29 02:09:30 +0100

    Chapter 2 - Adding a TrayItem to the system tray
    A system tray icon is shown when the clock view is shown, and when the
    clock view's bundle is stopped, is removed.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/Activator.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/Activator.java
@@ -10,6 +10,11 @@
 package com.packtpub.e4.clock.ui;
 
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Tray;
+import org.eclipse.swt.widgets.TrayItem;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -17,7 +22,8 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-
+	private TrayItem trayItem;
+	private Image image;
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.packtpub.e4.clock.ui"; //$NON-NLS-1$
 
@@ -39,6 +45,18 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		final Display display = Display.getDefault();
+		display.asyncExec(() -> {
+			image = new Image(display, Activator.class.getResourceAsStream("/icons/sample.gif"));
+			Tray tray = display.getSystemTray();
+			if (tray != null && image != null) {
+				trayItem = new TrayItem(tray, SWT.NONE);
+				trayItem.setToolTipText("Hello World");
+				trayItem.setVisible(true);
+				trayItem.setText("Hello World");
+				trayItem.setImage(image);
+			}
+		});
 	}
 
 	/*
@@ -49,7 +67,14 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
+		if (trayItem != null) {
+			Display.getDefault().asyncExec(trayItem::dispose);
+		}
+		if (image != null) {
+			Display.getDefault().asyncExec(image::dispose);
+		}
 		super.stop(context);
+
 	}
 
 	/**
