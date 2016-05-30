967535f4d801c466b6d4f90dcad26f8f31ad95c1
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-30 18:41:23 +0100

    Chapter 3 - TimeZoneTreeView with images
    This adds support to the ILabelProvider to return an associated image
    with the items as well, first using the platform's shared object image
    and then using custom descriptors.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
@@ -12,9 +12,20 @@ package com.packtpub.e4.clock.ui.views;
 import java.time.ZoneId;
 import java.util.Map;
 
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
 
 public class TimeZoneLabelProvider extends LabelProvider {
+	private final ISharedImages images;
+	private final ImageRegistry ir;
+
+	public TimeZoneLabelProvider(ISharedImages images, ImageRegistry ir) {
+		this.images = images;
+		this.ir = ir;
+	}
+
 	@SuppressWarnings("rawtypes")
 	public String getText(Object element) {
 		if (element instanceof Map) {
@@ -33,6 +44,16 @@ public class TimeZoneLabelProvider extends LabelProvider {
 			return (Object[]) inputElement;
 		} else {
 			return new Object[0];
+		}
+	}
+
+	public Image getImage(Object element) {
+		if (element instanceof Map.Entry) {
+			return images.getImage(ISharedImages.IMG_OBJ_FOLDER);
+		} else if (element instanceof ZoneId) {
+			return ir.get("sample");
+		} else {
+			return super.getImage(element);
 		}
 	}
 }
--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
@@ -9,22 +9,38 @@
  */
 package com.packtpub.e4.clock.ui.views;
 
+import java.net.URL;
+
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
+import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
 
 import com.packtpub.e4.clock.ui.internal.TimeZoneComparator;
 
 public class TimeZoneTreeView {
 	private TreeViewer treeViewer;
+	@Inject
+	private ISharedImages images;
 
 	@PostConstruct
 	public void create(Composite parent) {
+		ResourceManager rm = JFaceResources.getResources();
+		LocalResourceManager lrm = new LocalResourceManager(rm, parent);
+		ImageRegistry ir = new ImageRegistry(lrm);
+		URL sample = getClass().getResource("/icons/sample.gif");
+		ir.put("sample", ImageDescriptor.createFromURL(sample));
 		treeViewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI);
-		treeViewer.setLabelProvider(new TimeZoneLabelProvider());
+		treeViewer.setLabelProvider(new TimeZoneLabelProvider(images, ir));
 		treeViewer.setContentProvider(new TimeZoneContentProvider());
 		treeViewer.setInput(new Object[] { TimeZoneComparator.getTimeZones() });
 	}
