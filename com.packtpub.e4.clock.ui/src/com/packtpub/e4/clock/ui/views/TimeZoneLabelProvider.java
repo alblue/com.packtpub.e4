4e8d58b2ed527ba125c377ba02204f7ceb72cb0d
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-30 19:12:59 +0100

    Chapter 3 - TimeZoneTreeView with fonts
    This adds support to the ILabelProvider to display items in a
    different font, using the IFontProvider interface.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
@@ -10,20 +10,30 @@
 package com.packtpub.e4.clock.ui.views;
 
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Map;
 
+import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
+import org.eclipse.jface.viewers.IFontProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.ISharedImages;
 
-public class TimeZoneLabelProvider extends LabelProvider {
+public class TimeZoneLabelProvider extends LabelProvider implements IStyledLabelProvider, IFontProvider {
 	private final ISharedImages images;
 	private final ImageRegistry ir;
+	private final FontRegistry fr;
 
-	public TimeZoneLabelProvider(ISharedImages images, ImageRegistry ir) {
+	public TimeZoneLabelProvider(ISharedImages images, ImageRegistry ir, FontRegistry fr) {
 		this.images = images;
 		this.ir = ir;
+		this.fr = fr;
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -55,5 +65,21 @@ public class TimeZoneLabelProvider extends LabelProvider {
 		} else {
 			return super.getImage(element);
 		}
+	}
+
+	public StyledString getStyledText(Object element) {
+		String text = getText(element);
+		StyledString styledString = new StyledString(text);
+		if (element instanceof ZoneId) {
+			ZoneId zone = (ZoneId) element;
+			ZoneOffset offset = ZonedDateTime.now(zone).getOffset();
+			styledString.append(" (" + offset + ")", StyledString.DECORATIONS_STYLER);
+		}
+		return styledString;
+	}
+
+	public Font getFont(Object element) {
+		Font italic = fr.getItalic(JFaceResources.DEFAULT_FONT);
+		return italic;
 	}
 }
--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
@@ -15,11 +15,13 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -37,10 +39,11 @@ public class TimeZoneTreeView {
 		ResourceManager rm = JFaceResources.getResources();
 		LocalResourceManager lrm = new LocalResourceManager(rm, parent);
 		ImageRegistry ir = new ImageRegistry(lrm);
+		FontRegistry fr = new FontRegistry();
 		URL sample = getClass().getResource("/icons/sample.gif");
 		ir.put("sample", ImageDescriptor.createFromURL(sample));
 		treeViewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI);
-		treeViewer.setLabelProvider(new TimeZoneLabelProvider(images, ir));
+		treeViewer.setLabelProvider(new DelegatingStyledCellLabelProvider(new TimeZoneLabelProvider(images, ir, fr)));
 		treeViewer.setContentProvider(new TimeZoneContentProvider());
 		treeViewer.setInput(new Object[] { TimeZoneComparator.getTimeZones() });
 	}
