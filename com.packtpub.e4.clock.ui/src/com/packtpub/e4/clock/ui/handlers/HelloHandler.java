ff80edbb7f3f1e7dec8b4c8c789f8b29188b42e2
Alex Blewitt <alex.blewitt@gmail.com>
2016-05-31 08:35:55 +0100

    Chapter 4 - Running a job in the background
    The HelloHandler is updated to execute a 5s sleep, but since it runs
    in the background, it doesn't block the UI whilst doing so.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
@@ -9,12 +9,28 @@
  */
 package com.packtpub.e4.clock.ui.handlers;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.jface.dialogs.MessageDialog;
 
 public class HelloHandler {
 	@Execute
-	public void execute() {
-		MessageDialog.openInformation(null, "Hello", "World");
+	public void execute(final UISynchronize display) {
+		Job job = new Job("About to say hello") {
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+				}
+				display.asyncExec(() -> MessageDialog.openInformation(null, "Hello", "World"));
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+		return;
 	}
 }
