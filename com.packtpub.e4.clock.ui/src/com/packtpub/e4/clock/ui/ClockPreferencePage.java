bf5ce9a8b409c88f415c2ace706cc14898c82e34
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-01 22:02:36 +0100

    Chapter 5 - Storing the favourite time zone
    A combo field editor allows a set of values to be displayed, with
    a corresponding key that will be persisted in the preference store.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
@@ -9,7 +9,11 @@
  */
 package com.packtpub.e4.clock.ui;
 
+import java.time.ZoneId;
+import java.util.stream.Collectors;
+
 import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.ui.IWorkbench;
@@ -22,6 +26,10 @@ public class ClockPreferencePage extends FieldEditorPreferencePage implements IW
 		IntegerFieldEditor offset = new IntegerFieldEditor("offset", "Current offset from GMT", getFieldEditorParent());
 		offset.setValidRange(-14, +12);
 		addField(offset);
+		String[][] data = ZoneId.getAvailableZoneIds()//
+				.stream().sorted().map(s -> new String[] { s, s }) //
+				.collect(Collectors.toList()).toArray(new String[][] {});
+		addField(new ComboFieldEditor("favorite", "Favorite time zone", data, getFieldEditorParent()));
 	}
 
 	public void init(IWorkbench workbench) {
