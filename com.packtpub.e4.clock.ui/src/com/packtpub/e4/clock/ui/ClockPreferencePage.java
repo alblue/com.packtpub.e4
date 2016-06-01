f8ac625b58b0a60defa9deefd0515ea8965c4b50
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-01 19:44:22 +0100

    Chapter 5 - Creating a preference page
    Adds a ClockPreferencePage which shows the number of times the
    application has been launched.

---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.clock.ui;
+
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+public class ClockPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+	protected void createFieldEditors() {
+		addField(new IntegerFieldEditor("launchCount", "Number of times it has been launched", getFieldEditorParent()));
+	}
+
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(new ScopedPreferenceStore(InstanceScope.INSTANCE, "com.packtpub.e4.clock.ui"));
+	}
+}
