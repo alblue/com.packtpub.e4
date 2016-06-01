838e63629c7163954c88b1db1e5ef4b063b52e3e
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-01 22:10:30 +0100

    Chapter 5 - Add additional field editors to the preference page
    This adds different types of field editors to the preference page
    to demonstrate the the different capabilities.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
@@ -13,9 +13,16 @@ import java.time.ZoneId;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.ColorFieldEditor;
 import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.PathEditor;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.jface.preference.ScaleFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
@@ -34,6 +41,13 @@ public class ClockPreferencePage extends FieldEditorPreferencePage implements IW
 				.stream().sorted().map(s -> new String[] { s, s }) //
 				.collect(Collectors.toList()).toArray(new String[][] {});
 		addField(new ComboFieldEditor("favorite", "Favorite time zone", data, getFieldEditorParent()));
+		addField(new BooleanFieldEditor("tick", "Boolean value", getFieldEditorParent()));
+		addField(new ColorFieldEditor("colour", "Favourite colour", getFieldEditorParent()));
+		addField(new ScaleFieldEditor("scale", "Scale", getFieldEditorParent(), 0, 360, 10, 90));
+		addField(new FileFieldEditor("file", "Pick a file", getFieldEditorParent()));
+		addField(new DirectoryFieldEditor("dir", "Pick a directory", getFieldEditorParent()));
+		addField(new PathEditor("path", "Path", "Directory", getFieldEditorParent()));
+		addField(new RadioGroupFieldEditor("group", "Radio choices", 3, data, getFieldEditorParent(), true));
 	}
 
 	public void init(IWorkbench workbench) {
