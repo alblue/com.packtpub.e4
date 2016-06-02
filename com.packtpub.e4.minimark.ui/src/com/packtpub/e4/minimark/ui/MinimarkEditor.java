43ffd5f3518d297c43b772819c0ccfe7dd610162
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-02 21:14:17 +0100

    Chapter 6 - Creating an editor
    An editor is a subclass of IEditorPart (in Eclipse 3.x) and an
    associated extension point. This is enough to allow us to have a custom
    editor for text files with the 'minimark' extension.

---
+++ com.packtpub.e4.minimark.ui/src/com/packtpub/e4/minimark/ui/MinimarkEditor.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.minimark.ui;
+
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+
+public class MinimarkEditor extends AbstractTextEditor {
+	public MinimarkEditor() {
+		setDocumentProvider(new TextFileDocumentProvider());
+	}
+}
