3b3bc774e6bab8c8886e5578201cd18d9817c93d
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-05 22:42:06 +0100

    Chapter 7 - Passing in a parameter to a command
    This adds a parameter to a command, and wires it in to the parameter
    when it is called via the menu item.

--- com.packtpub.e4.application/src/com/packtpub/e4/application/handlers/HelloHandler.java
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/handlers/HelloHandler.java
@@ -9,11 +9,19 @@
  */
 package com.packtpub.e4.application.handlers;
 
+import javax.inject.Named;
+
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class HelloHandler {
 	@Execute
-	public void hello() {
-		System.out.println("Hello World");
+	public void hello(@Named(IServiceConstants.ACTIVE_SHELL) Shell s,
+			@Optional @Named("com.packtpub.e4.application.commandparameter.hello.value") String hello,
+			@Named("math.random") double value) {
+		MessageDialog.openInformation(s, "Hello World ", hello + value);
 	}
 }
