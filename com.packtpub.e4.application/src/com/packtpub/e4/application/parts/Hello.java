f547e02b696e51c21cbb78ac62183054a9928ba1
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-05 21:27:07 +0100

    Chapter 7 - Adding a Hello Part
    An Eclipse 4 application consists of Parts, which are defined in the
    Application.e4xmi file and as a POJO to display the part itself.
    
    Note that a clean of the application's workspace may be needed in order
    to pick up changes.

---
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.application.parts;
+
+import javax.annotation.PostConstruct;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class Hello {
+	private Label label;
+
+	@PostConstruct
+	public void create(Composite parent) {
+		label = new Label(parent, SWT.NONE);
+		label.setText("Hello");
+	}
+
+	@Focus
+	public void onFocus() {
+		label.setFocus();
+	}
+}
