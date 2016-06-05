6fe90a9c26e7bdc0273f6500086dcf61e467c9f3
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-05 22:13:25 +0100

    Chapter 7 - Running in the UI thread
    In E4, the UISynchronize class provides the same role as the
    Display.asyncExec() and Display.syncExec() methods. The difference is
    that it is not SWT specific and can be injected into components.

--- com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
@@ -13,12 +13,20 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.osgi.service.log.LogService;
@@ -32,9 +40,31 @@ public class Hello {
 	@Inject
 	@Named("math.random")
 	private Object random;
+	private Button button;
+	@Inject
+	private UISynchronize ui;
 
 	@PostConstruct
 	public void create(Composite parent) {
+		button = new Button(parent, SWT.PUSH);
+		button.setText("Do not push");
+		button.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				button.setEnabled(false);
+				new Job("Button Pusher") {
+					@Override
+					protected IStatus run(IProgressMonitor monitor) {
+						ui.asyncExec(() -> button.setEnabled(true));
+						return Status.OK_STATUS;
+					}
+				}.schedule(1000);
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
 		label = new Label(parent, SWT.NONE);
 		label.setText(window.getLabel() + " " + random);
 		logService.log(LogService.LOG_ERROR, "Hello");
