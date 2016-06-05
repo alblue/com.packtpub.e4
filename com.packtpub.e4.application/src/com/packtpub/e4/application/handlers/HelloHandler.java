602dd4950e0a4a9f2a64576a3a48d1bc5c5cebf1
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-05 22:30:08 +0100

    Chapter 7 - Add a menu item and a command/handler
    Commands and Handlers are roughly the same conceptually in Eclipse 4 as
    in Eclipse 3.x. However, instead of Eclipse 3.x where the plugin.xml is
    used to define the extensions, in E4 the e4xmi is used to define them.

---
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/handlers/HelloHandler.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.application.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class HelloHandler {
+	@Execute
+	public void hello() {
+		System.out.println("Hello World");
+	}
+}
