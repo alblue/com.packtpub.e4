ff296feb230ba61ffd6f3f589290d644d770a901
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-06 01:23:31 +0100

    Chapter 8 - Creating toolbars
    Toolbars can be created by working with the workspace model and the
    part model, and creating the toolbar code programmatically.

--- com.packtpub.e4.migration/src/com/packtpub/e4/migration/views/SampleView.java
+++ com.packtpub.e4.migration/src/com/packtpub/e4/migration/views/SampleView.java
@@ -9,9 +9,17 @@
  */
 package com.packtpub.e4.migration.views;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.menu.MDirectToolItem;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuFactory;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBarElement;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
@@ -27,6 +35,9 @@ import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
 public class SampleView {
+
+	@Inject
+	private MPart part;
 
 	/**
 	 * The ID of the view as specified by the extension.
@@ -60,6 +71,7 @@ public class SampleView {
 		// Create the help context id for the viewer's control
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "com.packtpub.e4.migration.viewer");
 		hookDoubleClickAction();
+		createToolBar();
 	}
 
 	private void hookDoubleClickAction() {
@@ -77,5 +89,21 @@ public class SampleView {
 	@Focus
 	public void setFocus() {
 		viewer.getControl().setFocus();
+	}
+
+	private void createToolBar() {
+		MDirectToolItem one = MMenuFactory.INSTANCE.createDirectToolItem();
+		one.setTooltip("Action 1 tooltip");
+		one.setIconURI("platform:/plugin/org.eclipse.ui/icons/full/obj16/info_tsk.png");
+		one.setContributionURI("bundleclass://com.packtpub.e4.migration/com.packtpub.e4.migration.views.HandlerOne");
+		MDirectToolItem two = MMenuFactory.INSTANCE.createDirectToolItem();
+		two.setTooltip("Action 2 tooltip");
+		two.setIconURI("platform:/plugin/org.eclipse.ui/icons/full/obj16/info_tsk.png");
+		two.setContributionURI("bundleclass://com.packtpub.e4.migration/com.packtpub.e4.migration.views.HandlerTwo");
+		MToolBar toolBar = MMenuFactory.INSTANCE.createToolBar();
+		List<MToolBarElement> children = toolBar.getChildren();
+		children.add(one);
+		children.add(two);
+		part.setToolbar(toolBar);
 	}
 }
