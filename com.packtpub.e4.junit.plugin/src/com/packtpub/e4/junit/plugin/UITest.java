fb83dbf68c9b73a95184c44808d7815ce0cef3a2
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-07 19:32:03 +0100

    Chapter 11 - Running tests with SWTBot
    SWTBot allows automated in-UI testing with Eclipse, and this tests shows
    what dependencies need to be added and the minimal test to support a
    test run.

---
+++ com.packtpub.e4.junit.plugin/src/com/packtpub/e4/junit/plugin/UITest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.junit.plugin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class UITest {
+	@Test
+	public void testUI() {
+		SWTWorkbenchBot bot = new SWTWorkbenchBot();
+		SWTBotShell[] shells = bot.shells();
+		boolean found = false;
+		for (int i = 0; i < shells.length && !found; i++) {
+			if (shells[i].isVisible()) {
+				if (shells[i].getText().contains("Eclipse")) {
+					found = true;
+				}
+			}
+		}
+		assertTrue(found);
+	}
+}
