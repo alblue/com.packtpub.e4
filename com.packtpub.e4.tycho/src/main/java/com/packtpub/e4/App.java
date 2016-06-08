01f6d9effce79e99da4ca0d06050c9711ff70f15
Alex Blewitt <alex.blewitt@gmail.com>
2016-06-08 23:24:15 +0100

    Chapter 12 - Generating a sample Tycho project
    This project was generated with the following command:
    
        mvn archetype:generate
         -DarchetypeGroupId=org.apache.maven.archetypes
         -DarchetypeArtifactId=maven-archetype-quickstart
         -DarchetypeVersion=1.1
         -DgroupId=com.packtpub.e4
         -DartifactId=com.packtpub.e4.tycho
         -Dversion=1.0-SNAPSHOT
    
    The project can be built by running:
    
        mvn clean package

---
+++ com.packtpub.e4.tycho/src/main/java/com/packtpub/e4/App.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ com.packtpub.e4.tycho/src/test/java/com/packtpub/e4/AppTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2016, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+}
